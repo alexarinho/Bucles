6d3ccee3c66d8fb89acadd3e461e39373abd3589
alumno <alumno@aula1-11>
2019-10-22 14:28:24 +0200

    Ejercicio4 Terminado

---
+++ src/inicio/Bucles4.java
@@ -0,0 +1,17 @@
+package inicio;
+
+public class Bucles4 {
+
+	public static void main(String[] args) {
+		System.out.println("Suma de los 100 primeros numeros");
+		int suma=0;
+		int contador=0;
+		
+		for(int i=1;i<=100;i++){
+			contador++;
+			suma = contador + suma;
+	}
+		System.out.println(suma);
+
+	}
+	}
