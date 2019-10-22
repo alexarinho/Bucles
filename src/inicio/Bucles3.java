479f009f940438dae53daa914ae0e11f3373b475
alumno <alumno@aula1-11>
2019-10-22 14:19:10 +0200

    Ejercicio 3 terminado

---
+++ src/inicio/Bucles3.java
@@ -0,0 +1,15 @@
+package inicio;
+
+public class Bucles3 {
+
+	public static void main(String[] args) {
+		System.out.println("Numeros pares entre 0 y 100");
+		
+		for(int i=1;i<=100;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
