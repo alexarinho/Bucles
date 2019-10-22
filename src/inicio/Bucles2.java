14bde36802b2dfdfde4a6f411c7073ea57f843d7
alumno <alumno@aula1-11>
2019-10-22 13:58:18 +0200

    Ejercicio2

---
+++ src/inicio/Bucles2.java
@@ -0,0 +1,13 @@
+package inicio;
+
+public class Bucles2 {
+
+	public static void main(String[] args) {
+		System.out.println("Numeros del 100 al 0");
+		
+		for(int i=100;i>=0;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
