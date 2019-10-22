6ef4b74add09758e522c4100e8431c39cc43607e
alumno <alumno@aula1-11>
2019-10-22 14:08:04 +0200

    Ejercicio 1 añadido

---
+++ src/inicio/Bucles.java
@@ -0,0 +1,17 @@
+package inicio;
+
+public class Bucles {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Numeos del 0 al 100");
+		int numero=0;
+		
+		for(int i=1;i<=101;i++){
+			System.out.println(numero++);
+			//Bucles 1
+		}
+
+	}
+
+}
